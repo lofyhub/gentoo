1d3cf9fd5392ba13a97f342ff5a12b3ae7ec1661
John Philip <developerphilo@gmail.com>
2023-01-12 13:32:40 +0300

    fix: signup functionality

--- src/components/icons/SpinnerIcon.vue
+++ src/components/icons/SpinnerIcon.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { withDefaults, defineProps } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    color?: string;
+  }>(),
+  {
+    color: `rgb(79 70 229)`,
+  }
+);
+</script>
+
 <template>
   <svg
     width="24"
@@ -12,12 +25,12 @@
       cx="12"
       cy="12"
       r="10"
-      stroke="currentColor"
+      :stroke="props.color"
       stroke-width="4"
     ></circle>
     <path
       class="opacity-100"
-      fill="currentColor"
+      :fill="props.color"
       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
     ></path>
   </svg>
