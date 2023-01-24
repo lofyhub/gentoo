078f0ce8be7dfd1bad8faac8c9e04b3b99cc5682
John Philip <developerphilo@gmail.com>
2023-01-24 15:33:36 +0300

    fix: whatsapp svg color to same

--- src/components/icons/socials/Whatsapp.vue
+++ src/components/icons/socials/Whatsapp.vue
@@ -5,7 +5,7 @@
     height="28"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="rgb(79 70 229)"
+    stroke="#818cf8"
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
