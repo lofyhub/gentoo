092edb4e87824825a5f7b713bd719cd42bdfb18f
ozark0 <developerphilo@gmail.com>
2023-02-01 20:50:37 +0300

    fix: remove info section

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -2,12 +2,10 @@
 import HomeIntro from "@/components/HomeIntro.vue";
 import HomeListings from "@/components/HomeListings.vue";
 import WhyUs from "@/components/WhyUs.vue";
-import InfoSection from "@/components/InfoSection.vue";
 </script>
 <template>
   <div class="home">
     <HomeIntro />
-    <InfoSection />
     <HomeListings />
     <WhyUs />
   </div>
